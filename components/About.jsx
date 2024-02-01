3e0c3c3968cc060df9cea7c207c8c48ae89f7c96
unknown <nirmalapuspa1324@gmail.com>
2024-02-01 11:35:22 +0700

    Skills - Need to revise

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ import {
   PhoneCall,
   GraduationCap,
   Calendar,
-  Briefcase,
+  Users,
 } from "lucide-react";
 import { info } from "autoprefixer";
 
@@ -47,7 +47,8 @@ const qualificationData = [
     data: [
       {
         school: "President University",
-        qualification: "Bachelor of Computer Science",
+        qualification:
+          "Bachelor of Computer Science - Proud to maintain a GPA of 3.88/4.00, and grateful for a 75% tuition fee scholarship from Jababeka.",
         years: "2021 - 2024 (Expected)",
       },
       {
@@ -62,12 +63,12 @@ const qualificationData = [
     data: [
       {
         activity: "Computing Gathering (COMPGATH) 2022",
-        qualification: "Student Supervisor",
-        year: "2022",
+        role: "During COMPGATH 2022, I served as a Student Supervisor, collaborating with a team to supervise and ensure the well-being of participants. My role included encouraging engagement and implementing safety measures.",
+        years: "2022",
       },
       {
         activity: "Social Project: Pengenalan Dasar Pemrograman Melalui Game",
-        qualification: "Head of Multimedia, Design, and Documentation Division",
+        role: "Heading the Multimedia, Design, and Documentation Division, I led a team of two in a social project introducing programming basics through games. This involved creating graphic components, contributing to event planning, and impacting 10+ children.",
         years: "2022",
       },
     ],
@@ -76,19 +77,64 @@ const qualificationData = [
 
 const skillData = [
   {
-    title: "skills",
+    title: "soft skills",
     data: [
       {
-        name: "HTML, CSS",
+        name: "Problem-solving",
       },
       {
-        name: "Front-end Development",
+        name: "Critical Thinking",
       },
       {
-        name: "JavaScript, PHP",
+        name: "Teamwork",
       },
       {
-        name: "Back-end Development",
+        name: "Leadership",
+      },
+      {
+        name: "Adaptability",
+      },
+      {
+        name: "Strategic Planning",
+      },
+      {
+        name: "Creativity",
+      },
+      {
+        name: "Communication",
+      },
+    ],
+  },
+  {
+    title: "hard skills",
+    data: [
+      {
+        name: "Version Control",
+        component: "Git, GitHub",
+      },
+      {
+        name: "Frameworks",
+        component: "Flask, React",
+      },
+      {
+        name: "Programming Language",
+        component: "Python, Java, SQL, JavaScript, PHP, C++, C#",
+      },
+      {
+        name: "Database",
+        component: "MongoDB (NoSQL), MySQL (SQL)",
+      },
+      {
+        name: "Office Tools",
+        component: "Microsoft Office (Word, Excel, PowerPoint)",
+      },
+      {
+        name: "Graphic Design",
+        component: "Photoshop, Adobe Illustrator (Beginner), ibisPaintX",
+      },
+      {
+        name: "3D Design & Game",
+        component: "Blender, Unity",
       },
     ],
   },
@@ -105,7 +151,7 @@ const skillData = [
         imgPath: "/about/notion.svg",
       },
       {
-        imgPath: "/about/wordpress.svg",
+        imgPath: "/about/blender.svg",
       },
     ],
   },
@@ -159,9 +205,7 @@ const About = () => {
                     <p className="subtitle max-w-xl mx-auto xl:mx-0">
                       I'm currently pursuing an Informatics degree at President
                       University, where my emphasis on critical thinking and
-                      effective communication is key. Proud to maintain a GPA of
-                      3.88/4.00, and grateful for a 75% tuition fee scholarship
-                      from Jababeka.
+                      effective communication is key.
                     </p>
                     {/* Icons */}
                     <div className="grid xl:grid-cols-2 gap-4 mb-12">
@@ -178,15 +222,153 @@ const About = () => {
                       })}
                     </div>
                     {/* languages */}
-                    <div>
-                      <div>Language</div>
+                    <div className="flex flex-col gap-y-2">
+                      <div className="text-primary">Language Skill</div>
+                      <div className="border-b border-border"></div>
+                      <div>
+                        Indonesian - <i>native</i> | English - <i>fluent</i>
+                      </div>
                     </div>
                   </div>
                 </TabsContent>
+                {/* Qualification */}
                 <TabsContent value="qualifications">
-                  Qualifications info
+                  <div>
+                    <h3 className="h3 mb-8 text-center xl:text-left">
+                      My Journey
+                    </h3>
+                    {/* Experience & education wrapper */}
+                    <div className="grid md:grid-cols-2 gap-y-8">
+                      {/* Education */}
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <GraduationCap size={28} />
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationData, "education").title}
+                          </h4>
+                        </div>
+                        {/* List */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "education").data.map(
+                            (item, index) => {
+                              const { school, qualification, years } = item;
+                              return (
+                                <div className="flex gap-x-8 group" key={index}>
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-4">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {school}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
+                      {/* Leadership Experiences */}
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <Users />
+                          <h4 className="capitalize font-medium">
+                            {
+                              getData(
+                                qualificationData,
+                                "leadership experiences"
+                              ).title
+                            }
+                          </h4>
+                        </div>
+                        {/* List */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(
+                            qualificationData,
+                            "leadership experiences"
+                          ).data.map((item, index) => {
+                            const { activity, role, years } = item;
+                            return (
+                              <div className="flex gap-x-8 group" key={index}>
+                                <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                  <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                </div>
+                                <div>
+                                  <div className="font-semibold text-xl leading-none mb-2">
+                                    {activity}
+                                  </div>
+                                  <div className="text-lg leading-none text-muted-foreground mb-4">
+                                    {role}
+                                  </div>
+                                  <div className="text-base font-medium">
+                                    {years}
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </TabsContent>
-                <TabsContent value="skills">Skills info</TabsContent>
+                {/* Skills */}
+                <TabsContent value="skills">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">Tools I Use Everyday</h3>
+                    {/* soft skills */}
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* Skills List */}
+                      <div>
+                        {getData(skillData, "soft skills").data.map(
+                          (item, index) => {
+                            const { name } = item;
+                            return (
+                              <ul
+                                className="w-2/4 text-center xl:text-left mx-auto xl:mx-0"
+                                key={index}
+                              >
+                                <li className="font-medium">{name}</li>
+                              </ul>
+                            );
+                          }
+                        )}
+                      </div>
+                    </div>
+                    {/* tools */}
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">
+                        Tools
+                      </h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* tools list */}
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, "tools").data.map((item, index) => {
+                          const { imgPath } = item;
+                          return (
+                            <div key={index}>
+                              <Image
+                                src={imgPath}
+                                width={48}
+                                height={48}
+                                alt=""
+                                priority
+                              />
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                </TabsContent>
               </div>
             </Tabs>
           </div>
